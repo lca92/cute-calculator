b69f811ab73661302abfc0c4db3b4898d47a458a
Lena Araújo <lenaaraujo92@gmail.com>
2024-04-04 16:31:05 -0300

    js testing

---
+++ calculator.js
@@ -0,0 +1,10 @@
+// const integers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const operations = ["sum", "subtraction", "multiplication", "division"];
+
+// //  To bind values to the buttons
+// const zero = document.getElementById("#zero_up");
+// const zeroButton = document.getElementById("#zero_button");
+
+// zeroButton.addEventListener("click", () => {
+//   zero.src = "./images/division_down.svg";
+// });
